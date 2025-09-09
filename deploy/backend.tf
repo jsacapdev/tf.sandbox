b95860602979f099a2564123a5daaa0749d1a67b
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2025-09-09 11:21:37 +0100

    8d272a00-cc29-4db1-9a1a-83c7ba66ca81

--- deploy/backend.tf
+++ deploy/backend.tf
@@ -1,10 +1,6 @@
-# deploy/backend.tf
-
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-tf-shared-01"
-    storage_account_name = "sttfshared11"
-    container_name       = "tfstate"
-    key                  = "api-integration-iac.tfstate"
+    use_oidc             = true
+    use_azuread_auth     = true
   }
 }
